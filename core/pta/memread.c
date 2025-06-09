a14678f1ece4af101ba7148c002e5a6b2a7ce96a
Fabian Schmitt <fabian.schmitt@stud-mail.uni-wuerzburg.de>
2025-06-09 19:01:30 +0200

    fix: allow bigger VM memory sizes

--- core/pta/memread.c
+++ core/pta/memread.c
@@ -13,7 +13,7 @@
 
 #define PTA_NAME "memread.pta"
 
-//#define MEMREAD_THIS_VM_ID      0
+#define PTA_MEMREAD_MEM_BLOCK_SIZE 0x200000
 
 
 struct vm_mem_mapping {
@@ -41,86 +41,6 @@ struct vm_mem_mapping_config config = {
 };
 
 
-/* int get_shared_ipc(struct vm_config* vm1, struct vm_config* vm2) {
-    for (size_t i = 0; i < vm1->platform.ipc_num; i++) {
-        for (size_t j = 0; j < vm2->platform.ipc_num; j++) {
-            if (vm1->platform.ipcs[i].shmem_id == vm2->platform.ipcs[j].shmem_id)
-                return vm1->platform.ipcs[i].shmem_id;
-        }
-    }
-
-    for (size_t i = 0; i < vm1->children_num; i++) {
-        int res = get_shared_ipc(vm1->children[i], vm2);
-        if (res >= 0)
-            return res;
-    }
-
-    for (size_t i = 0; i < vm2->children_num; i++) {
-        int res = get_shared_ipc(vm1, vm2->children[i]);
-        if (res >= 0)
-            return res;
-    }
-
-    for (size_t i = 0; i < vm1->children_num; i++) {
-        for (size_t j = 0; j < vm2->children_num; j++) {
-            int res = get_shared_ipc(vm1->children[i], vm2->children[j]);
-            if (res >= 0)
-                return res;
-        }
-    }
-
-    return -1;
-}
-
-int get_shmem_base_addr(struct vm_config* vm, size_t shmem_id) {
-    for (size_t ipc_idx = 0; ipc_idx < vm->platform.ipc_num; ipc_idx++) {
-        struct ipc ipc_ = vm->platform.ipcs[ipc_idx];
-
-        if (ipc_.shmem_id == shmem_id)
-            return ipc_.base;
-    }
-
-    return -1;
-}
-
-int get_shmem_size(struct vm_config* vm, size_t shmem_id) {
-    for (size_t ipc_idx = 0; ipc_idx < vm->platform.ipc_num; ipc_idx++) {
-        struct ipc ipc_ = vm->platform.ipcs[ipc_idx];
-
-        if (ipc_.shmem_id == shmem_id)
-            return ipc_.size;
-    }
-
-    return -1;
-}
-
-int get_vm_memory_mapping(size_t vm_idx, struct vm_mem_mapping* cfg) {
-    if (vm_idx >= config.vmlist_size) {
-        return -1;
-    }
-
-    struct vm_config* this_vm = config.vmlist[MEMREAD_THIS_VM_ID];
-    struct vm_config* other_vm = config.vmlist[vm_idx];
-
-    int shared_ipc = get_shared_ipc(this_vm, other_vm);
-    if (shared_ipc < 0)
-        return -1;
-
-    int shared_ipc_base = get_shmem_base_addr(this_vm, shared_ipc);
-    if (shared_ipc_base < 0)
-        return -1;
-
-    int shared_ipc_size = get_shmem_size(this_vm, shared_ipc);
-    if (shared_ipc_size < 0)
-        return -1;
-
-    cfg->phy = shared_ipc_base;
-    cfg->size = shared_ipc_size;
-
-    return 0;
-} */
-
-
 TEE_Result get_pattern_base_address_in_memory_region(
     size_t memory_start,
     size_t memory_size,
@@ -144,6 +64,55 @@ TEE_Result get_pattern_base_address_in_memory_region(size_t memory_start, size_t
 }
 
 
+TEE_Result work_on_memory_block(size_t start_memory_address, const char* memory_pattern, size_t memory_pattern_size, size_t area_to_hash_total_size, bool* pattern_found, void* hash_ctx, size_t* memory_offset) {
+    TEE_Result res;
+    TEE_Result res_mem;
+    void* virtual_start_addr;
+    size_t block_offset;
+
+    virtual_start_addr = core_mmu_add_mapping(MEM_AREA_IO_NSEC, start_memory_address, PTA_MEMREAD_MEM_BLOCK_SIZE);
+
+    if (virtual_start_addr == NULL)
+        virtual_start_addr = phys_to_virt(MEM_AREA_IO_NSEC, start_memory_address, PTA_MEMREAD_MEM_BLOCK_SIZE);
+
+    if (virtual_start_addr == NULL)
+        return TEE_ERROR_OUT_OF_MEMORY;
+
+    char* block = (char*) virtual_start_addr;
+    block_offset = 0;
+
+    if (!(*pattern_found)) { // pattern not found, looking for matches in memory
+        for (block_offset = 0; block_offset < PTA_MEMREAD_MEM_BLOCK_SIZE; block_offset++) {
+            if (block[block_offset] == memory_pattern[*memory_offset]) {
+                (*memory_offset)++;
+
+                if (*memory_offset >= memory_pattern_size) {
+                    *pattern_found = true;
+                    break;
+                }
+            } else {
+                *memory_offset = 0;
+            }
+        }
+    }
+
+    if (*pattern_found) { // pattern was found, hash the entire or remaining part of the block
+        size_t bytes_to_hash =
+            (area_to_hash_total_size - *memory_offset) >= (PTA_MEMREAD_MEM_BLOCK_SIZE - block_offset)
+                ? PTA_MEMREAD_MEM_BLOCK_SIZE - block_offset
+                : area_to_hash_total_size - *memory_offset;
+
+        res = crypto_hash_update(hash_ctx, (const uint8_t*) block + block_offset, bytes_to_hash);
+        *memory_offset += bytes_to_hash;
+    }
+
+    res_mem = core_mmu_remove_mapping(MEM_AREA_IO_NSEC, virtual_start_addr, PTA_MEMREAD_MEM_BLOCK_SIZE);
+    if (res == TEE_SUCCESS)
+        return res_mem;
+    return res;
+}
+
+
 static TEE_Result invoke_command(
     void* sess_ctx __maybe_unused,
     uint32_t cmd_id __maybe_unused,
@@ -160,9 +129,9 @@ static TEE_Result invoke_command(
     size_t output_buf_size;
 
     struct vm_mem_mapping map;
-    void* vaddr;
-    char* memory_region_under_test;
     struct crypto_hash_ctx* ctx;
+    bool pattern_found;
+    size_t memory_offset;
 
     uint32_t exp_param_types = TEE_PARAM_TYPES(
         TEE_PARAM_TYPE_VALUE_INPUT,
@@ -187,19 +156,6 @@ static TEE_Result invoke_command(
     if (vm_index >= config.mappings_num)
         return TEE_ERROR_BAD_PARAMETERS;
     map = config.mappings[vm_index];
-    /*if (get_vm_memory_mapping(vm_index, &map) < 0)
-        return TEE_ERROR_BAD_PARAMETERS;*/
-
-    vaddr = core_mmu_add_mapping(MEM_AREA_IO_NSEC, map.phy, map.size);
-    if (vaddr == NULL)
-        vaddr = phys_to_virt(map.phy, MEM_AREA_IO_NSEC, 1);
-
-    if (vaddr == NULL)
-        return TEE_ERROR_OUT_OF_MEMORY;
-
-    res = get_pattern_base_address_in_memory_region((size_t) vaddr, map.size, input_pattern, input_pattern_size, memory_region_under_test_size, &memory_region_under_test);
-    if (res != TEE_SUCCESS)
-        return res;
 
     ctx = NULL;
     res = crypto_hash_alloc_ctx((void**) &ctx, TEE_ALG_SHA512);
@@ -210,9 +166,25 @@ static TEE_Result invoke_command(
     if (res != TEE_SUCCESS)
         goto out;
 
-    res = crypto_hash_update(ctx, (const uint8_t*) memory_region_under_test, memory_region_under_test_size);
+    res = crypto_hash_update(ctx, (const uint8_t*) input_pattern, input_pattern_size);
     if (res != TEE_SUCCESS)
         goto out;
+
+    pattern_found = false;
+    memory_offset = 0;
+    for (size_t block_offset = 0; block_offset < map.phy / PTA_MEMREAD_MEM_BLOCK_SIZE; block_offset++) {
+        res = work_on_memory_block(
+            block_offset * PTA_MEMREAD_MEM_BLOCK_SIZE,
+            input_pattern, input_pattern_size,
+            memory_region_under_test_size,
+            &pattern_found,
+            ctx,
+            &memory_offset
+        );
+
+        if (memory_offset >= memory_region_under_test_size)
+            break;
+    }
 
     res = crypto_hash_final(ctx, (uint8_t*) output_buf, output_buf_size);
 
